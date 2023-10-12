04c12e2ee24a9575b416cae1f9544b159021134d
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-12 20:16:01 -0300

    Creating a new sub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CardLabelComponent } from './components/card/card-label/card-label.comp
     HomeComponent,
     CardComponent,
     MenuBarComponent,
-    CardLabelComponent
+    CardLabelComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
