ef2fc0c754c963a8af4570fb37e24d422f6ea464
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-11 21:39:22 -0300

    Reseting the css and add border-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
